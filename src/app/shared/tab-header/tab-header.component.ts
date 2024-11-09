0ce229a622cf5c8bf8a5c706851165505ac73938
Ice <ice.baldivas@gmail.com>
2024-11-08 23:09:57 -0600

    add header component for tabs

---
+++ src/app/shared/tab-header/tab-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-header',
+  templateUrl: './tab-header.component.html',
+  styleUrls: ['./tab-header.component.scss'],
+})
+export class TabHeaderComponent {
+  @Input() title: string = '';
+
+  constructor() {}
+}
