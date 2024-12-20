0d14653fe46ce93e47fde25ab9baeb3167cdd107
Ice <ice.baldivas@gmail.com>
2024-12-20 17:57:35 -0600

    twisty for 2x2

--- src/app/shared/twisty/twisty.component.ts
+++ src/app/shared/twisty/twisty.component.ts
@@ -1,18 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
+import type { Puzzle } from 'src/app/types/puzzle';
 
 @Component({
   selector: 'app-twisty',
   templateUrl: './twisty.component.html',
   styleUrls: ['./twisty.component.scss'],
 })
-export class TwistyComponent {
+export class TwistyComponent implements OnInit {
   scriptElement: HTMLScriptElement;
   @Input() sequence: string = "R U R' U R U2' R'";
   @Input() setupAlgo: string = 'x2 y2';
-  @Input() puzzleType: '3x3x3' | '2x2x3' = '3x3x3';
+  @Input({ required: true }) puzzleType: Puzzle = null;
 
   constructor() {
     this.scriptElement = document.createElement('script');
+  }
+
+  ngOnInit(): void {
     this.scriptElement.src = 'https://cdn.cubing.net/v0/js/cubing/twisty';
     this.scriptElement.type = 'module';
     document.body.appendChild(this.scriptElement);
